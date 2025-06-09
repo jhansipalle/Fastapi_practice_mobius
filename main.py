1c0d41cd85138588118d10fce118a23d679afc0f
pjhansireddy23 <pjhansireddy23@gmail.com>
2025-06-09 14:04:25 +0530

    Add update and delete task

--- main.py
+++ main.py
@@ -28,3 +28,25 @@ def create_todo(todo: Todo):
 def get_all_todos():
     return {"tasks": todos}
 
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: Todo):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            todo["title"] = updated_todo.title
+            todo["completed"] = updated_todo.completed
+            return {
+                "message": "Task updated successfully!",
+                "task": todo
+            }
+    return {"error": "Task not found!"}
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(todos):
+        if todo["id"] == todo_id:
+            deleted = todos.pop(index)
+            return {
+                "message": "Task deleted successfully!",
+                "task": deleted
+            }
+    return {"error": "Task not found!"}
