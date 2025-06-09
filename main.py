ce708f28b508b3e931558361b059e9e860d56532
pjhansireddy23 <pjhansireddy23@gmail.com>
2025-06-09 12:27:53 +0530

    Add unique id to each task

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ class Todo(BaseModel):
     completed: bool = False
 
 todos = []
+todo_id_counter = 1
 
+# Create a new todo item
 @app.post("/todos")
 def create_todo(todo: Todo):
-    todos.append(todo)
-    return {"message": "Task added successfully!", "task": todo}
+    global todo_id_counter
+    todo_with_id={
+        "id": todo_id_counter,
+        "title": todo.title,
+        "completed": todo.completed
+    }
+    todos.append(todo_with_id)
+    todo_id_counter += 1
+    return {"message": "Task added successfully!",
+             "task": todo_with_id}
 
 @app.get("/todos")
 def get_all_todos():
